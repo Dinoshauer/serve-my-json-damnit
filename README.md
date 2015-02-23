392a24d7d099b031535911f65ee4ff41b55b9570
Kasper Jacobsen <k@mackwerk.dk>
2015-02-23 21:54:53 +0100

    ITS ALIVE! ITS ALIVE!

---
+++ README.md
@@ -0,0 +1,19 @@
+Super duper easy server
+=======================
+
+**Disclaimer:** My JavaScript skills are to say the least, lacking.
+
+However, I have on more than one occasion had a friend in dire need
+of a quick and very dirty server that will return a snippet of JSON,
+while prototyping a client.
+
+This should do just that.
+
+*Usage:*
+
+    $ [PROGRAM_NAME_HERE] /path/to/my.json
+    listening on: 5000
+
+*JSON format:*
+
+Look in [`example.json`][example.json] for a super fast example, showing everything supported.
--- index.js
+++ index.js
@@ -1,9 +1,76 @@
 var fp = process.argv[2],
     fs = require('fs'),
+    _ = require('lodash'),
     restify = require('restify');
 
 var server = restify.createServer(),
-    config = JSON.parse(fs.readFileSync(fp));
+    config = JSON.parse(fs.readFileSync(fp)),
+    port = config.port || 5000;
+
+var method_map = {
+    GET: function (server, url, code, data, headers) {
+        server.get(url, function (req, res, next) {
+            res.headers = headers || {};
+            res.send(code, data);
+            return next();
+        });
+    },
+    DELETE: function (server, url, code, data, headers) {
+        server.del(url, function (req, res, next) {
+            res.headers = headers || {};
+            res.send(code, data);
+            return next();
+        });
+    },
+    HEAD: function (server, url, code, data, headers) {
+        server.head(url, function (req, res, next) {
+            res.headers = headers || {};
+            res.send(code, data);
+            return next();
+        });
+    },
+    OPTIONS: function (server, url, code, data, headers) {
+        server.opts(url, function (req, res, next) {
+            res.headers = headers || {};
+            res.send(code, data);
+            return next();
+        });
+    },
+    PATCH: function (server, url, code, data, headers) {
+        server.patch(url, function (req, res, next) {
+            res.headers = headers || {};
+            res.send(code, data);
+            return next();
+        });
+    },
+    POST: function (server, url, code, data, headers) {
+        server.post(url, function (req, res, next) {
+            res.headers = headers || {};
+            res.send(code, data);
+            return next();
+        });
+    },
+    PUT: function (server, url, code, data, headers) {
+        server.put(url, function (req, res, next) {
+            res.headers = headers || {};
+            res.send(code, data);
+            return next();
+        });
+    },
+}
 
 
-server.listen(5000);
+
+_.forEach(config.endpoints, function(endpoint) {
+    var method = endpoint.method,
+        url = endpoint.url,
+        code = endpoint.code,
+        response = endpoint.response,
+        headers = endpoint.headers;
+
+    method_map[method](server, url, code, response);
+});
+
+server.listen(port, function () {
+    console.log('listening on: %s', port)
+});
