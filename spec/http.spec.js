a16e47e7374e85153d3990b58b512de67cdfcd0a
Kasper Jacobsen <k@mackwerk.dk>
2015-02-24 19:51:03 +0100

    More tests

--- spec/http.spec.js
+++ spec/http.spec.js
@@ -21,12 +21,64 @@ describe('Serve my JSON, damnit!', function () {
             });
         });
 
-        it('should return a list of users', function (done) {
+        it('accepts a GET request', function (done) {
             request.get('http://localhost:8000/users', function (e, res, body) {
                 var expected = [
                         {id: 0, name: 'Foo', email: 'foo@example.com'},
                         {id: 1, name: 'Bar', email: 'bar@example.com'}
                     ],
+                    result = JSON.parse(body);
+                expect(result).toEqual(expected);
+                expect(res.statusCode).toBe(200);
+                done();
+            });
+        });
+
+        afterEach(function (done) {
+            http._server.close();
+            done();
+        });
+    });
+    describe('POST', function (done) {
+        beforeEach(function (done) {
+            var fp = path.resolve(__dirname, 'res', 'post.json'),
+                config = JSON.parse(fs.readFileSync(fp));
+
+            http.init(config, function() {
+                done();
+            });
+        });
+
+        it('accepts a POST request', function (done) {
+            var d = {foo: 'bar'};
+            request.post('http://localhost:8000/users', d, function (e, res, body) {
+                var expected = {status: 'success!'},
+                    result = JSON.parse(body);
+                expect(result).toEqual(expected);
+                expect(res.statusCode).toBe(200);
+                done();
+            });
+        });
+
+        afterEach(function (done) {
+            http._server.close();
+            done();
+        });
+    });
+    describe('PUT', function (done) {
+        beforeEach(function (done) {
+            var fp = path.resolve(__dirname, 'res', 'put.json'),
+                config = JSON.parse(fs.readFileSync(fp));
+
+            http.init(config, function() {
+                done();
+            });
+        });
+
+        it('accepts a PUT request', function (done) {
+            var d = {foo: 'bar'};
+            request.put('http://localhost:8000/users/0', d, function (e, res, body) {
+                var expected = {status: 'success!'},
                     result = JSON.parse(body);
                 expect(result).toEqual(expected);
                 expect(res.statusCode).toBe(200);
